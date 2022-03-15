9f037391dfb501bd1c92068784c8a007ffd9ae07
Kowsar Atazadeh <kowsar.atazadeh@gmail.com>
2022-03-16 02:37:41 +0330

    Add handleTransfer

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,12 +1,27 @@
+import { Transfer } from './../generated/ERC721/ERC721';
+import { log } from "@graphprotocol/graph-ts";
 import { OrdersMatched } from "../generated/OpenseaExchange/OpenseaExchange"
-import { OrdersMatchedEntity } from "./schema"
+import { Sale } from "./schema"
 
 export function handleOrdersMatched(event: OrdersMatched): void {
-  const id = event.transaction.hash.toHex() + "-" + event.logIndex.toString();
-  let entity = OrdersMatchedEntity.load(id);
-  if (!entity) {
-    return; // TODO: warn
+  const id = event.transaction.hash.toHexString();
+  let entity: Sale | null = Sale.load(id);
+  if (entity != null) {
+    log.warning("Sale entity already exists: " + id, []);
+    return;
   }
-  entity = new OrdersMatchedEntity(id, event);
+  entity = new Sale(id, event);
   entity.save();
+}
+
+export function handleTransfer(event: Transfer): void {
+  const id = event.transaction.hash.toHexString();
+  const entity: Sale | null = Sale.load(id);
+  if (entity == null) {
+    log.warning("Transfer event ignored since no OrdersMatched event handled for this event: " + id, []);
+    return;
+  }
+  entity.saveTokenData(event);
+  entity.save();
+  log.info("Sale entity saved: " + id, []);
 }
--- src/schema.ts
+++ src/schema.ts
@@ -1,4 +1,5 @@
 import { OrdersMatched } from '../generated/OpenseaExchange/OpenseaExchange';
+import { Transfer } from '../generated/ERC721/ERC721';
 
 import {
   Entity,
@@ -9,34 +10,36 @@ import {
   BigInt,
 } from "@graphprotocol/graph-ts";
 
-export class OrdersMatchedEntity extends Entity {
+export class Sale extends Entity {
 
   constructor(id: string, event: OrdersMatched) {
     super();
     this.id = id;
-    this.transactionHash = event.transaction.hash;
-    this.buyHash = event.params.buyHash;
-    this.sellHash = event.params.sellHash;
-    this.maker = event.params.maker;
-    this.taker = event.params.taker;
-    this.metadata = event.params.metadata;
+    this.blockNumber = event.block.number;
     this.price = event.params.price;
+  }
+
+  saveTokenData(event: Transfer): void {
+    this.seller = event.params.from;
+    this.buyer = event.params.to;
+    this.tokenId = event.params.tokenId;
+    this.collection = event.address;
   }
 
   save(): void {
     let id = this.get("id");
-    assert(id != null, "Cannot save OrdersMatchedEntity entity without an ID");
+    assert(id != null, "Cannot save Sale entity without an ID");
     if (id) {
       assert(
         id.kind == ValueKind.STRING,
-        `Entities of type OrdersMatchedEntity must have an ID of type String but the id '${id.displayData()}' is of type ${id.displayKind()}`
+        `Entities of type Sale must have an ID of type String but the id '${id.displayData()}' is of type ${id.displayKind()}`
       );
-      store.set("OrdersMatchedEntity", id.toString(), this);
+      store.set("Sale", id.toString(), this);
     }
   }
 
-  static load(id: string): OrdersMatchedEntity | null {
-    return changetype<OrdersMatchedEntity | null>(store.get("OrdersMatchedEntity", id));
+  static load(id: string): Sale | null {
+    return changetype<Sale | null>(store.get("Sale", id));
   }
 
   get id(): string {
@@ -48,58 +51,22 @@ export class OrdersMatchedEntity extends Entity {
     this.set("id", Value.fromString(value));
   }
 
-  get transactionHash(): Bytes {
-    let value = this.get("transactionHash");
+  get seller(): Bytes {
+    let value = this.get("seller");
     return value!.toBytes();
   }
 
-  set transactionHash(value: Bytes) {
-    this.set("transactionHash", Value.fromBytes(value));
+  set seller(value: Bytes) {
+    this.set("seller", Value.fromBytes(value));
   }
 
-  get buyHash(): Bytes {
-    let value = this.get("buyHash");
+  get buyer(): Bytes {
+    let value = this.get("buyer");
     return value!.toBytes();
   }
 
-  set buyHash(value: Bytes) {
-    this.set("buyHash", Value.fromBytes(value));
-  }
-
-  get sellHash(): Bytes {
-    let value = this.get("sellHash");
-    return value!.toBytes();
-  }
-
-  set sellHash(value: Bytes) {
-    this.set("sellHash", Value.fromBytes(value));
-  }
-
-  get maker(): Bytes {
-    let value = this.get("maker");
-    return value!.toBytes();
-  }
-
-  set maker(value: Bytes) {
-    this.set("maker", Value.fromBytes(value));
-  }
-
-  get taker(): Bytes {
-    let value = this.get("taker");
-    return value!.toBytes();
-  }
-
-  set taker(value: Bytes) {
-    this.set("taker", Value.fromBytes(value));
-  }
-
-  get metadata(): Bytes {
-    let value = this.get("metadata");
-    return value!.toBytes();
-  }
-
-  set metadata(value: Bytes) {
-    this.set("metadata", Value.fromBytes(value));
+  set buyer(value: Bytes) {
+    this.set("buyer", Value.fromBytes(value));
   }
 
   get price(): BigInt {
@@ -109,5 +76,32 @@ export class OrdersMatchedEntity extends Entity {
 
   set price(value: BigInt) {
     this.set("price", Value.fromBigInt(value));
+  }
+
+  get blockNumber(): BigInt {
+    let value = this.get("blockNumber");
+    return value!.toBigInt();
+  }
+
+  set blockNumber(value: BigInt) {
+    this.set("blockNumber", Value.fromBigInt(value));
+  }
+
+  get tokenId(): BigInt {
+    let value = this.get("tokenId");
+    return value!.toBigInt();
+  }
+
+  set tokenId(value: BigInt) {
+    this.set("tokenId", Value.fromBigInt(value));
+  }
+
+  get collection(): Bytes {
+    let value = this.get("collection");
+    return value!.toBytes();
+  }
+
+  set collection(value: Bytes) {
+    this.set("collection", Value.fromBytes(value));
   }
 }
